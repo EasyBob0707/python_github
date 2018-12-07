5fde63d08fda38dec5149fc3d1efcd29b55e0e95
sunbo <sunbo930516@foxmail.com>
2018-12-07 11:37:09 +0800

    python 格式化
    Change-Id: I4e2ef400b0a10ddc9d8601c79dd0219f868a8e9f

---
+++ demo_01/com/python/demo/Formatting.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+print('Hello, %s' % 'world')
+print('Hi, %s, you have $%d.' % ('Michael', 1000000))
+print('%2d-%02d' % (3, 1))
+print('%.2f' % 3.1415926)
